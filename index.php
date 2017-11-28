6d39aee1f91ad6c4fb7aeba3ff6fe84a921c5593
Alexis <alexis_dussart@hotmail.fr>
2017-11-28 10:28:15 +0100

    Dépot Initial de mon Thème

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+    /**
+     * Ici, je suis sur le fichier par défaut pour tout thème wordpress.
+     * Il est l'un des deux obligatoire (L'autre étant le style.css)
+     * Il est utilisé pour afficher une page si aucun autre fichier n'est disponible.
+     * https://developer.wordpress.org/themes/basics/template-hierarchy/
+     */
+    get_header();
+
+    get_footer();
+
+?>
